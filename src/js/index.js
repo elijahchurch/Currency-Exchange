fea3169280a4073367e42263c10b317031c86a0c
Jason E. Church <elijahchurch@gmail.com>
2023-06-30 10:48:35 -0700

    create framework for UI logic. Test API call for response.

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,32 @@
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "../css/styles.css";
-import //Galaxycalc from './galactic';
+import exchangeService from './exchangeService';
+
+function processExchangeData() {
+    exchangeService.getExchangeRates()
+    // .then(function (response) {
+    //     if (response.conversion_rates) {
+    //         printResult(response);
+    //     } else {
+    //         printError(response)
+    //     }
+    // });
+}
+
+// function printResult(response) {
+
+// }
+
+// function printError(response) {
+
+// }
+
+function handleForm(event) {
+    event.preventDefault();
+    processExchangeData();
+
+}
+
+document.querySelector("#convertForm").addEventListener("submit", handleForm);
 
