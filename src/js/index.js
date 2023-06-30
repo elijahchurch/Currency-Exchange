309bbbbfc2cf6716848def8986e6cf6ee5f41d2e
Jason E. Church <elijahchurch@gmail.com>
2023-06-30 11:09:52 -0700

    update UI logic for several functions. Test values for form with console log

--- src/js/index.js
+++ src/js/index.js
@@ -7,23 +7,28 @@ function processExchangeData() {
     exchangeService.getExchangeRates()
     // .then(function (response) {
     //     if (response.conversion_rates) {
-    //         printResult(response);
+    //         printResult(response, dollarInput, currencyInput);
     //     } else {
     //         printError(response)
     //     }
     // });
 }
 
-// function printResult(response) {
+// let resultStatement = document.querySelector("#result");
+
+// function printResult(response, dollarInput, currencyInput) {
 
 // }
 
 // function printError(response) {
-
+//     resultStatement.innerText = `We were unable to process the data at this time because of an error: ${response}`;
 // }
 
 function handleForm(event) {
     event.preventDefault();
+    let dollarInput = document.querySelector("#USD").value
+    let currencyInput = document.querySelector("#currencies").value
+    console.log(dollarInput, currencyInput);
     processExchangeData();
 
 }
